2d0e93b4fe1958d453fb32929366c5730aec02b4
aidizhang <aidizhang@college.harvard.edu>
2014-11-01 10:10:20 -0400

    Trying to download Google Maps SDK

--- SMS Boomerang/SMS Boomerang/AppDelegate.m
+++ SMS Boomerang/SMS Boomerang/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "GoogleMaps.framework/Headers/GoogleMaps.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [GMSServices provideAPIKey:@"AIzaSyA9rIM32g9vYnWX6F_hvmvUEzffeCj5ZyI"];
     return YES;
 }
 
--- SMS Boomerang/SMS Boomerang/ViewController.m
+++ SMS Boomerang/SMS Boomerang/ViewController.m
@@ -1,5 +1,5 @@
 //
-//  ViewController.m
+//  ViewController.m/Users/idzhang/Documents/smsboomerang/SMS Boomerang/SMS Boomerang.xcodeproj
 //  SMS Boomerang
 //
 //  Created by Roger on 10/31/14.
@@ -7,16 +7,34 @@
 //
 
 #import "ViewController.h"
+//#import "GoogleMaps.framework/Headers/GoogleMaps.h"
+#import <GoogleMaps/GoogleMaps.h>
 
 @interface ViewController ()
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    GMSMapView *mapView_;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    // Create a GMSCameraPosition that tells the map to display the coordinates -33.86, 151.20 with zoom level 6
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
+                                                            longitude:151.20
+                                                                 zoom:6];
+    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    mapView_.myLocationEnabled = YES;
+    self.view = mapView_;
+    
+    // Creates a marker in the center of the map
+    GMSMarker *marker = [[GMSMarker alloc] init];
+    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
+    marker.title = @"Sydney";
+    marker.snippet = @"Australia";
+    marker.map = mapView_;
 }
 
 - (void)didReceiveMemoryWarning {
